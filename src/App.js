f969604bcd34a741575baeddad118e6e9185a6c8
Puyan Wei <puyanwei@gmail.com>
2018-06-14 17:26:16 +0100

    Used event listener

--- src/App.js
+++ src/App.js
@@ -25,11 +25,12 @@ class StreetFighter extends Component {
             ],
         };
     }
-
     render() {
         let characters = this.state.characters;
         characters = characters.map((char, index) => {
-            return <Character char={char} key={index} />;
+            return (
+                <Character char={char} key={index} onDelete={this.onDelete.bind} />
+            );
         });
         return (
             <div>
@@ -38,18 +39,42 @@ class StreetFighter extends Component {
             </div>
         );
     }
+    onDelete(chosenCharacter) {
+        let updatedCharactersList = this.state.characters.filter(
+            (char, index) => {
+                return chosenCharacter !== char;
+            }
+        );
+        // setState is like vuex mutation
+        this.setState({
+            characters: updatedCharactersList,
+        });
+    }
 }
 
 class Character extends Component {
+    constructor(props){
+        super(props)
+        this.handleDelete = this.handleDelete.bind(this);
+    }
     render() {
         return (
             <li>
                 <div className="character">
-                    <span className="characterName">{this.props.char}</span>
+                    <span className="character-name">{this.props.char}</span>
+                    <span
+                        className="character-delete"
+                        onClick={this.handleDelete}
+                        > x </span>
                 </div>
             </li>
-        );
+        )
+    };
+
+    handleDelete() {
+        this.props.onDelete(this.props.char);
     }
 }
+
 
 export default App;
